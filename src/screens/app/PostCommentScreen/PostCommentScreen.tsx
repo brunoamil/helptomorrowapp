7c6229e47c0ff62a73c617dd20a2ec3207c4148b
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-01-14 15:35:35 -0300

    hotfix: change types for Post Id

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// import {usePaginatedList} from '@domain';
+import {usePostCommentList} from 'src/domain/PostComment/useCases/usePostCommentList';
+
 import {Box, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
@@ -7,6 +10,8 @@ export function PostCommentScreen({
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
   const {postId} = route.params;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const {list} = usePostCommentList(postId);
 
   return (
     <Screen canGoBack title="Comentário">
