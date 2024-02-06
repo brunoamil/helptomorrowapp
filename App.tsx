7de89935f567cb98059aa4b5925b8d98e5667388
bruno-amil@hotmail.com <bruno-amil@hotmail.com>
2024-02-05 21:35:20 -0300

    feat: create implementation with mmkvstorage

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import {AuthCredentialsProvider, ToastProvider} from '@services';
+import {
+  AuthCredentialsProvider,
+  MMKVStorage,
+  ToastProvider,
+  initiliazeStorage,
+} from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {QueryClient, QueryClientProvider} from 'react-query';
@@ -10,6 +15,7 @@ import {Toast} from '@components';
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
+initiliazeStorage(MMKVStorage);
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
