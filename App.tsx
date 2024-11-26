8bc7d15d616136611f364ef2bf588a75bad8f301
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-11-26 11:41:21 -0300

    feat: create componente screen and safeareacontext

--- App.tsx
+++ App.tsx
@@ -1,43 +1,16 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
-import {TextInput} from './src/components/TextInput/TextInput';
-import {Icon} from './src/components/Icon/Icon';
+import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App(): React.JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <View style={{paddingHorizontal: 24}}>
-          <Text mb="s8" preset="headingLarge">
-            Olá!
-          </Text>
-          <Text preset="paragraphLarge" mb="s40">
-            Digite seu e-mail e senha para entrar
-          </Text>
-          <TextInput
-            errorMessage="E-mail inválido"
-            label="E-mail"
-            placeholder="Digite seu e-mail"
-            boxProps={{mb: 's20'}}
-          />
-          <TextInput
-            RightComponent={<Icon name="eyeOn" color="gray2" />}
-            label="Senha"
-            placeholder="Digite sua senha"
-            boxProps={{mb: 's10'}}
-          />
-          <Text mt="s10" color="primary" preset="paragraphSmall" bold>
-            Esqueci minha senha
-          </Text>
-          <Button title="Entrar" mt="s48" />
-          <Button title="Criar uma conta" mt="s10" preset="outline" />
-        </View>
-      </SafeAreaView>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
---
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {Icon} from '../../../components/Icon/Icon';
+import {Button} from '../../../components/Button/Button';
+import {Screen} from '../../../components/Screen/Screen';
+
+export function LoginScreen() {
+  return (
+    <Screen>
+      <Text mb="s8" preset="headingLarge">
+        Olá!
+      </Text>
+      <Text preset="paragraphLarge" mb="s40">
+        Digite seu e-mail e senha para entrar
+      </Text>
+      <TextInput
+        errorMessage="E-mail inválido"
+        label="E-mail"
+        placeholder="Digite seu e-mail"
+        boxProps={{mb: 's20'}}
+      />
+      <TextInput
+        RightComponent={<Icon name="eyeOn" color="gray2" />}
+        label="Senha"
+        placeholder="Digite sua senha"
+        boxProps={{mb: 's10'}}
+      />
+      <Text mt="s10" color="primary" preset="paragraphSmall" bold>
+        Esqueci minha senha
+      </Text>
+      <Button title="Entrar" mt="s48" />
+      <Button title="Criar uma conta" mt="s10" preset="outline" />
+    </Screen>
+  );
+}
