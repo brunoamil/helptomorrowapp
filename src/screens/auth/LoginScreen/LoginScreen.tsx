10d6afb7192a5d48df30f851ec598726c6f95deb
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-11-26 20:42:48 -0300

    feat: use hook form at login

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -6,9 +6,23 @@ import {Screen} from '../../../components/Screen/Screen';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../routes/Routes';
+import {useForm, Controller} from 'react-hook-form';
+import {Alert} from 'react-native';
 
+type LoginFormType = {
+  email: string;
+  password: string;
+};
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 export function LoginScreen({navigation}: ScreenProps) {
+  const {control, formState, handleSubmit} = useForm<LoginFormType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
   function navigateToSignUpScreen() {
     navigation.navigate('SignUpScreen');
   }
@@ -17,6 +31,9 @@ export function LoginScreen({navigation}: ScreenProps) {
     navigation.navigate('ForgotPasswordScreen');
   }
 
+  function submitForm({email, password}: LoginFormType) {
+    Alert.alert('Login', `Email: ${email} Senha: ${password}`);
+  }
   return (
     <Screen>
       <Text mb="s8" preset="headingLarge">
@@ -25,17 +42,50 @@ export function LoginScreen({navigation}: ScreenProps) {
       <Text preset="paragraphLarge" mb="s40">
         Digite seu e-mail e senha para entrar
       </Text>
-      <TextInput
-        errorMessage="E-mail inválido"
-        label="E-mail"
-        placeholder="Digite seu e-mail"
-        boxProps={{mb: 's20'}}
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: 'E-mail é obrigatório',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: 'E-mail inválido',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <TextInput
+            value={field.value}
+            errorMessage={fieldState.error?.message}
+            onChangeText={field.onChange}
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
+        )}
       />
-      <PasswordInput
-        label="Senha"
-        placeholder="Digite sua senha"
-        boxProps={{mb: 's10'}}
+
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: 'Senha é obrigatória',
+          minLength: {
+            value: 3,
+            message: 'Senha precisa ter pelo menos 3 caracteres',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <PasswordInput
+            value={field.value}
+            errorMessage={fieldState.error?.message}
+            onChangeText={field.onChange}
+            label="Senha"
+            placeholder="Digite sua senha"
+            boxProps={{mb: 's10'}}
+          />
+        )}
       />
+
       <Text
         onPress={navigateToForgotPasswordScreen}
         mt="s10"
@@ -44,7 +94,12 @@ export function LoginScreen({navigation}: ScreenProps) {
         bold>
         Esqueci minha senha
       </Text>
-      <Button title="Entrar" mt="s48" />
+      <Button
+        title="Entrar"
+        mt="s48"
+        onPress={handleSubmit(submitForm)}
+        disabled={!formState.isValid}
+      />
       <Button
         onPress={navigateToSignUpScreen}
         title="Criar uma conta"
