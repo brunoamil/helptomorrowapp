3825dab225b649ebfb111f1f419c4a6c8f053167
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-11-27 21:46:33 -0300

    feat: configuratio navigation tab

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -6,7 +6,7 @@ import {AppStack} from './AppStack';
 import {AuthStack} from './AuthStack';
 
 export function Router() {
-  const authentication = false;
+  const authentication = true;
 
   return (
     <NavigationContainer>
