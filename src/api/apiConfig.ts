a97f21c0437d28b53902d7ffb44e9425e7eb7d01
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-01 15:35:19 -0300

    feat: create service for page and commentsPost

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://localhost:3333/',
   headers: {
     Authorization:
-      'Bearer Nw.uIxtFWCmy4hMf0FoL6N3U8Yw9YcXAh6gbYPHG1UMG7qwoK-y3-2sE4qBONdB',
+      'Bearer MQ.1QdZ5zs-bM6ctmN9wHDQzr74zuD6E8qrWlCNoBaKsgpX3S6NjTJawj2bEzFc',
   },
 });
---
+++ src/domain/PostComment/postCommentService.ts
@@ -0,0 +1,26 @@
+import {apiAdapter} from '@api';
+import {Page} from '@types';
+
+import {postCommentAdapter} from './postCommentAdapter';
+import {postCommentApi} from './postCommentApi';
+import {PostComment} from './postCommentTypes';
+
+const PER_PAGE = 10;
+async function getList(
+  postId: number,
+  page: number,
+): Promise<Page<PostComment>> {
+  const postCommentAPI = await postCommentApi.getList(postId, {
+    page,
+    per_page: PER_PAGE,
+  });
+
+  return {
+    data: postCommentAPI.data.map(postCommentAdapter.toPostComment),
+    meta: apiAdapter.toMetaDataPage(postCommentAPI.meta),
+  };
+}
+
+export const postCommentService = {
+  getList,
+};
