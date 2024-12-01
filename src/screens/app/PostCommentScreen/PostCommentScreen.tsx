c92c5d237dab0806b955b04ea8068c5300e127b6
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-01 17:28:06 -0300

    feat: create component postComment

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
+import {FlatList, ListRenderItemInfo} from 'react-native';
 
-import {usePostCommentList} from 'src/domain/PostComment/useCases/usePostCommentList';
+import {PostComment, usePostCommentList} from '@domain';
 
-import {Box, Screen, Text} from '@components';
+import {Screen} from '@components';
+import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
+
+import {PostCommentBottom, PostCommentItem} from './components';
 
 export function PostCommentScreen({
   route,
 }: AppScreenProps<'PostCommentScreen'>) {
+  const {bottom} = useAppSafeArea();
   const postId = route.params.postId;
-  const {list} = usePostCommentList(postId);
+  const {list, fetchNextPage, hasNextPage} = usePostCommentList(postId);
 
+  function renderItem({item}: ListRenderItemInfo<PostComment>) {
+    return <PostCommentItem postComment={item} />;
+  }
   return (
     <Screen title="Comentários" canGoBack>
-      <Box>
-        <Text>Tela de Comentários</Text>
-      </Box>
+      <FlatList
+        data={list}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: bottom + 15}}
+        ListFooterComponent={
+          <PostCommentBottom
+            fetchNextPage={fetchNextPage}
+            hasNextPage={hasNextPage}
+          />
+        }
+      />
     </Screen>
   );
 }
