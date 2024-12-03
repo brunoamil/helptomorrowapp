dc2f0eee26c070fd425ead80c46a1fc80319821b
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-02 22:09:11 -0300

    feat: implemetation zustand with toast

--- src/services/toast/useToast.tsx
+++ src/services/toast/useToast.tsx
@@ -1,7 +1,29 @@
-import {useToastZustand} from './Providers/useToastZustand';
-import {ToastService} from './toastTypes';
-import {useToastContext} from './useToastContext';
+// import {Toast} from '@components';
 
-export function useToast(): ToastService {
+import {
+  useToastServiceZustand,
+  useToastZustand,
+} from './Providers/useToastZustand';
+import {ToastService} from './toastTypes';
+// import {useToastContext} from './useToastContext';
+
+export function useToast(): ToastService['toast'] {
+  //context
+  // const {toast} = useToastContext();
+  // return toast;
   return useToastZustand();
+}
+
+export function useToastService(): Pick<
+  ToastService,
+  'showToast' | 'hideToast'
+> {
+  //context
+  // const {showToast, hideToast} = useToastContext();
+  // return {
+  //   showToast,
+  //   hideToast,
+  // };
+
+  return useToastServiceZustand();
 }
