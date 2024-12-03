f2d3a0087430cfb8aedeccc1897c5d2a7581b6f2
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-02 23:56:37 -0300

    feat: create user usecases

---
+++ src/domain/User/useCases/useUserGetById.ts
@@ -0,0 +1,34 @@
+import {useCallback, useEffect, useState} from 'react';
+
+import {userService} from '../userService';
+import {User} from '../userTypes';
+
+export function useUserGetById(id: number) {
+  const [user, setUser] = useState<User>();
+  const [error, setError] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const getUserById = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const _user = await userService.getById(id);
+      setUser(_user);
+    } catch (e) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    getUserById();
+  }, [getUserById]);
+
+  return {
+    user,
+    error,
+    loading,
+    getUserById,
+  };
+}
