8ee5ec4ec642812b2de17f8fe5144d13be44d212
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-06 19:52:24 -0300

    feat: create interceptor with axios

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,3 +1,4 @@
+import {AuthCredentials, authService} from '@domain';
 import axios from 'axios';
 
 export const api = axios.create({
@@ -7,3 +8,44 @@ export const api = axios.create({
   //     'Bearer MQ.1QdZ5zs-bM6ctmN9wHDQzr74zuD6E8qrWlCNoBaKsgpX3S6NjTJawj2bEzFc',
   // },
 });
+
+type InterceptorProps = {
+  authCredentials: AuthCredentials | null;
+  removeCredentials: () => Promise<void>;
+  saveCredentials: (ac: AuthCredentials) => Promise<void>;
+};
+export function registerInterceptor({
+  authCredentials,
+  removeCredentials,
+  saveCredentials,
+}: InterceptorProps) {
+  const interceptor = api.interceptors.response.use(
+    response => response,
+    async responseError => {
+      const failedRequest = responseError.config;
+      const hasNotRefreshToken = !authCredentials?.refreshToken;
+      const isRefreshTokenRequest =
+        authService.isRefreshTokenRequest(failedRequest);
+
+      if (responseError.response.status === 401) {
+        if (hasNotRefreshToken || isRefreshTokenRequest || failedRequest.sent) {
+          removeCredentials();
+          return Promise.reject(responseError);
+        }
+
+        failedRequest.sent = true;
+
+        const newAuthCredentials = await authService.authenticateByRefreshToken(
+          authCredentials.refreshToken,
+        );
+        saveCredentials(newAuthCredentials);
+
+        failedRequest.headers.Authorization = `Bearer ${newAuthCredentials.token}`;
+        return api(failedRequest);
+      }
+      return Promise.reject(responseError);
+    },
+  );
+  //remove listen quando o componente é desmontado
+  return () => api.interceptors.response.eject(interceptor);
+}
