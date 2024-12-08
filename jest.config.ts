fd4d625ce7c639f6c99794243a5a176de4e0003f
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-08 20:32:57 -0300

    test: configuration jest.ts

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+// module.exports = {
+//   preset: 'react-native',
+//   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
+//   collectCoverageFrom: [
+//     'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+//   ],
+//   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+//   moduleDirectories: ['node_modules', './src/test'],
+//   modulePathIgnorePatterns: ['.*/mockedData/.*'],
+//   setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
+//   transformIgnorePatterns: [
+//     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
+//   ],
+// };
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'react-native',
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleDirectories: ['node_modules', './src/test'],
+  modulePathIgnorePatterns: ['.*/mockedData/.*'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: ['/node_modules/', 'index'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-safe-area-context)/)',
+  ],
+};
+
+export default config;
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,7 +2,11 @@ import React, {ReactElement} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientConfig,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import {
   render,
   renderHook,
@@ -12,26 +16,27 @@ import {
 
 import {theme} from '@theme';
 
+const queryClientConfig: QueryClientConfig = {
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    // ✅ no more errors on the console for tests
+    //@ts-ignore
+    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+  },
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+};
 export const wrapperAllTheProviders = () => {
-  const queryClient = new QueryClient({
-    logger: {
-      log: console.log,
-      warn: console.warn,
-      // ✅ no more errors on the console for tests
-      //@ts-ignore
-      error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
-    },
-    defaultOptions: {
-      queries: {
-        retry: false,
-        cacheTime: Infinity,
-      },
-      mutations: {
-        retry: false,
-        cacheTime: Infinity,
-      },
-    },
-  });
+  const queryClient = new QueryClient(queryClientConfig);
   return ({children}: {children: React.ReactNode}) => (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
@@ -46,6 +51,24 @@ function customRender<T = unknown>(
   options?: Omit<RenderOptions, 'wrapper'>,
 ) {
   return render(component, {wrapper: wrapperAllTheProviders(), ...options});
+}
+
+export const wrapperScreenProvider = () => {
+  const queryClient = new QueryClient(queryClientConfig);
+  return ({children}: {children: React.ReactNode}) => (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+export function renderScreen<T = unknown>(
+  component: ReactElement<T>,
+  options?: Omit<RenderOptions, 'wrapper'>,
+) {
+  return render(component, {wrapper: wrapperScreenProvider(), ...options});
 }
 
 function customRenderHook<Result, Props>(
