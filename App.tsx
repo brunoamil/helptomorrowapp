43d02004bf353717ae6615036093b75745cef734
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-08 09:45:45 -0300

    build-config react native config

--- App.tsx
+++ App.tsx
@@ -8,9 +8,10 @@ import {
 } from '@services';
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import Config from 'react-native-config';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-import {Toast} from '@components';
+import {Box, Text, Toast} from '@components';
 
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
@@ -24,6 +25,9 @@ function App(): React.JSX.Element {
         <SafeAreaProvider>
           <ThemeProvider theme={theme}>
             <ToastProvider>
+              <Box>
+                <Text>{Config.API_URL}</Text>
+              </Box>
               <Router />
               <Toast />
             </ToastProvider>
