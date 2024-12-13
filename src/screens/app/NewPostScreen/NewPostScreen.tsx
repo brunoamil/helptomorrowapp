b091d679450acf8441bafb90dcaa8ecc4ea759f8
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-12 22:42:16 -0300

    feat: style for gallery images

--- src/screens/app/NewPostScreen/NewPostScreen.tsx
+++ src/screens/app/NewPostScreen/NewPostScreen.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
 import {useCameralRoll} from '@services';
 
-import {Screen, Text} from '@components';
+import {Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+import {Header} from './components/Header';
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+const NUM_COLUMNS = 4;
+const ITEM_WIDTH = SCREEN_WIDTH / 4;
 export function NewPostScreen({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   navigation,
 }: AppTabScreenProps<'NewPostScreen'>) {
   const {list} = useCameralRoll();
+
+  function renderItem({item}: ListRenderItemInfo<string>) {
+    return (
+      <Image
+        key={item}
+        source={{uri: item}}
+        style={{width: ITEM_WIDTH, height: ITEM_WIDTH}}
+      />
+    );
+  }
   return (
-    <Screen scrollable>
-      <Text preset="headingSmall">New Post Screen</Text>
-      {list.map(photo => (
-        <Image
-          key={photo}
-          source={{uri: photo}}
-          style={{width: 200, height: 200}}
-        />
-      ))}
+    <Screen canGoBack title="Novo post" noPaddingHorizontal>
+      <FlatList
+        data={list}
+        renderItem={renderItem}
+        numColumns={NUM_COLUMNS}
+        ListHeaderComponent={
+          <Header imageUri={list[0]} imageWidth={SCREEN_WIDTH} />
+        }
+      />
     </Screen>
   );
 }
