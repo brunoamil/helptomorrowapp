4ff8be47ff09640940c3f84a0f905460e08eaa24
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-15 19:04:12 -0300

    feat: send image for backend

--- src/domain/Post/useCases/usePostCreate.ts
+++ src/domain/Post/useCases/usePostCreate.ts
@@ -27,14 +27,16 @@ export function usePostCreate(options?: MutationOptions<Post>) {
     },
   });
 
-  function createPost({
+  async function createPost({
     description,
     imageUri,
   }: {
     description: string;
     imageUri: string;
   }) {
-    const imageForUpload = multiMediaService.prepareImageForUpload(imageUri);
+    const imageForUpload = await multiMediaService.prepareImageForUpload(
+      imageUri,
+    );
 
     mutate({text: description, imageCover: imageForUpload});
   }
--- src/services/multiMedia/multiMediaService.ts
+++ src/services/multiMedia/multiMediaService.ts
@@ -1,4 +1,7 @@
+import {Platform} from 'react-native';
+
 import {CameraRoll} from '@react-native-camera-roll/camera-roll';
+import {manipulateAsync, SaveFormat} from 'expo-image-manipulator';
 
 import {ImageForUpload, PhotoListPaginated} from './multiMediaTypes';
 
@@ -17,14 +20,34 @@ async function getPhotos(cursor?: string): Promise<PhotoListPaginated> {
   };
 }
 
-function prepareImageForUpload(imageUri: string): ImageForUpload {
+async function prepareImageForUpload(
+  imageUri: string,
+): Promise<ImageForUpload> {
+  console.log('...imageUri', imageUri);
+  const image = await manipulateAsync(prepareImageUri(imageUri), [], {
+    compress: 0.5,
+    format: SaveFormat.JPEG,
+  });
   return {
-    uri: imageUri,
-    name: 'name',
-    type: 'image/png',
+    uri: image.uri,
+    name: Date.now().toString(),
+    type: 'image/jpeg',
   };
+}
+
+function prepareImageUri(imageUri: string): string {
+  if (Platform.OS !== 'android') {
+    return imageUri;
+  }
+
+  if (imageUri.startsWith('file://')) {
+    return imageUri;
+  }
+
+  return `file://${imageUri}`;
 }
 export const multiMediaService = {
   prepareImageForUpload,
   getPhotos,
+  prepareImageUri,
 };
