08141cf961a1b9f0e8b744ff5cb503c443b6e3bd
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-21 23:49:29 -0300

    feat: comentarios screen

--- src/screens/app/FavoriteScreen/FavoriteScreen.tsx
+++ src/screens/app/FavoriteScreen/FavoriteScreen.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-import {InfinityScrollList, Screen, Text} from '@components';
+import {Box, InfinityScrollList, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
-import { Image, ListRenderItemInfo } from 'react-native';
+import { Dimensions, Image, ListRenderItemInfo } from 'react-native';
 import { QueryKeys } from '@infra';
 import { PostReaction, postReactionService } from '@domain';
 
+
+const NUM_COLUMNS = 2;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_PADDING = 24;
+const ITEM_MARGIN = 16;
+
+const ITEM_WIDTH = (SCREEN_WIDTH - SCREEN_PADDING * 2) / NUM_COLUMNS;
 export function FavoriteScreen({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   navigation,
@@ -13,20 +20,28 @@ export function FavoriteScreen({
   
   function renderItem({item}: ListRenderItemInfo<PostReaction>) {
     return (
-      <Image 
+      <Box>
+        <Image 
         source={{ uri: item.post.imageURL}}
-        style={{ width: 300, height: 300}}
+        style={{ width: ITEM_WIDTH, height: ITEM_WIDTH}}
       />
+      <Text semiBold mt="s4">{item.author.username}</Text>
+      </Box>
     )
   }
   return (
-    <Screen flex={1}>
-      <Text preset="headingSmall">Favorite Screen</Text>
+    <Screen flex={1} title='Favoritos'>
       <InfinityScrollList 
       getList={(page) => postReactionService.getMyReactions('favorite', page)}
       queryKey={[QueryKeys.FavoriteList]}
       renderItem={renderItem}
-      
+      flatListProps={{
+        numColumns: NUM_COLUMNS,
+        columnWrapperStyle: {columnGap: ITEM_MARGIN},
+        contentContainerStyle: {
+          rowGap: SCREEN_PADDING,
+        }
+      }}
       emptyListProps={{
         emptyMessage: 'Não há favoritos',
         errorMessage: 'Erro ao carregar favoritos'
