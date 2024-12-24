16ed3622453f9d7528be6c03d7128dc68bc60576
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-24 12:21:23 -0300

    fix: comments

--- src/screens/app/NewPostScreen/components/Header.tsx
+++ src/screens/app/NewPostScreen/components/Header.tsx
@@ -22,6 +22,7 @@ export function Header({imageUri, imageWidth}: Props) {
   function navigateToCamera() {
     navigation.navigate('CameraScreen');
   }
+
   return (
     <Box>
       <ImageBackground
