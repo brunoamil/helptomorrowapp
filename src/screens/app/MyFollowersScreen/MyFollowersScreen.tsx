0b54d704b34c2e00e18a22e14982ebb0e25cc959
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2024-12-29 18:07:12 -0300

    test

--- src/screens/app/MyFollowersScreen/MyFollowersScreen.tsx
+++ src/screens/app/MyFollowersScreen/MyFollowersScreen.tsx
@@ -20,7 +20,7 @@ export function MyFollowersScreen({}: AppScreenProps<'MyFollowersScreen'>) {
   return (
     <UserListTemplate
       screenTitle="Seguidores"
-      emptyMessage="Você ainda não está sendo seguido por ninguém"
+      emptyMessage="Você ainda não está sendo seguido por ninguéms"
       totalText="seguidores"
       queryKey={QueryKeys.MyFollowersList}
       getUserList={followService.getMyFollowersList}
