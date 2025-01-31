8d42351a9afbf0c5cf11f7306bb3f08dcc254fd6
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-01-31 08:20:40 -0300

    feat: test in export

--- src/domain/Follow/followApi.ts
+++ src/domain/Follow/followApi.ts
@@ -61,3 +61,18 @@ export const followApi = {
   getMyFollowingList,
   getMyFollowersList,
 };
+
+export const followApi = {
+  isFollowing,
+  followUser,
+  removeFollow,
+  getMyFollowingList,
+  getMyFollowersList,
+};
+export const followApi = {
+  isFollowing,
+  followUser,
+  removeFollow,
+  getMyFollowingList,
+  getMyFollowersList,
+};
