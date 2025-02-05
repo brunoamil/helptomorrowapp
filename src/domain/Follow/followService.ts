0f26c6b2577693e268461585b838826ecfbcda13
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-05 15:00:27 -0300

    fix: removed text

--- src/domain/Follow/followService.ts
+++ src/domain/Follow/followService.ts
@@ -51,37 +51,3 @@ export const followService = {
   followUser,
   removeFollow,
 };
-
-//
-async function getMyFollowersList(page: number): Promise<Page<FollowUser>> {
-  const followPageAPI = await followApi.getMyFollowersList({
-    page,
-    per_page: 10,
-  });
-
-  return apiAdapter.toPageModel(
-    followPageAPI,
-    followAdapter.fromFollowerToUser,
-  );
-}
-
-async function isFollowing(userId: string): Promise<{isFollowing: boolean}> {
-  return followApi.isFollowing(userId);
-}
-
-async function followUser(userId: number): Promise<User> {
-  const data = await followApi.followUser(userId);
-  return followAdapter.fromFollowingToUser(data);
-}
-
-async function removeFollow(followId: number): Promise<void> {
-  await followApi.removeFollow(followId);
-}
-
-export const followService = {
-  getMyFollowingList,
-  getMyFollowersList,
-  isFollowing,
-  followUser,
-  removeFollow,
-};
