64e4fe988fd14559e28f75a7870ab669cb9e1937
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-08 22:56:54 -0300

    test hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -19,3 +19,17 @@ export function useDebounce<T>(value: T, delay = 500): T {
 
   return debouncedValue;
 }
+
+export function useDebounce<T>(value: T, delay = 500): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
