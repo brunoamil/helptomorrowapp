44177f4645db2f013ba529e8ff7e48764f04bd42
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-19 23:27:45 -0300

    feat: fix to interface post

--- src/domain/PostComment/postCommentTypes.ts
+++ src/domain/PostComment/postCommentTypes.ts
@@ -22,3 +22,25 @@ export interface PostCommentAPI {
   user: UserAPI;
   meta: any;
 }
+
+export interface PostCommentAPI {
+  id: number;
+  message: string;
+  user_id: number;
+  post_id: number;
+  created_at: string;
+  updated_at: string;
+  user: UserAPI;
+  meta: any;
+}
+
+export interface PostCommentAPI {
+  id: number;
+  message: string;
+  user_id: number;
+  post_id: number;
+  created_at: string;
+  updated_at: string;
+  user: UserAPI;
+  meta: any;
+}
