3919e7f97ec956419ee8bcaa2faf7428d00af7a9
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-20 22:04:09 -0300

    TEST

--- src/domain/PostComment/postCommentTypes.ts
+++ src/domain/PostComment/postCommentTypes.ts
@@ -22,26 +22,3 @@ export interface PostCommentAPI {
   user: UserAPI;
   meta: any;
 }
-
-export interface PostComment {
-  id: number;
-  message: string;
-  createdAt: string;
-  createdRelative: string;
-  author: {
-    id: number;
-    profileURL: string;
-    name: string;
-    userName: string;
-  };
-}
-export interface PostCommentAPI {
-  id: number;
-  message: string;
-  user_id: number;
-  post_id: number;
-  created_at: string;
-  updated_at: string;
-  user: UserAPI;
-  meta: any;
-}
