2c4d0e1d32b26e7c7fa5a68b0bd02832d44f0272
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-03-15 09:25:03 -0300

    feat: addiing new actitivy indicator

--- src/components/ActivityIndicatorPrev/ActivityIndicator.tsx
+++ src/components/ActivityIndicatorPrev/ActivityIndicator.tsx
@@ -19,5 +19,11 @@ export function ActivityIndicator({color = 'primary', ...rest}: Props) {
       color={colors[color]}
       {...rest}
     />
+    <Box>
+      <RNActivityIndicator
+      testID="activity-indicator"
+      color={colors[color]}
+      {...rest}
+    />
   );
 }
